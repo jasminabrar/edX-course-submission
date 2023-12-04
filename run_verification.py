04bd7d8419c02c66e736e514411eb2ae622a76a6
jasminabrar <jasminabrarr@gmail.com>
2023-12-04 15:51:39 -0800

    use gds file in repo instead of hardcoded name

--- run_verification.py
+++ run_verification.py
@@ -8,7 +8,8 @@ import os
 import siepic_ebeam_pdk
 
 # gds file to run verification on
-gds_file = "ebeam_adiabatic_te1550.gds"
+#gds_file = "ebeam_adiabatic_te1550.gds"
+gds_file = os.environ.get('GDS_FILE')
 
 # load into layout
 layout = pya.Layout()
@@ -28,13 +29,13 @@ layout.TECHNOLOGY = get_technology_by_name('EBeam')
 zoom_out(top_cell)
 
 path = os.path.dirname(os.path.realpath(__file__))
-filename = 'ebeam_adiabatic_te1550'
+# filename = str(gds_file)
 
-print('SiEPIC_EBeam_PDK: ebeam_adiabatic_te1550 - verification')
-file_lyrdb = os.path.join(path,filename+'.lyrdb')
+print('SiEPIC_EBeam_PDK: {} - verification'.format(gds_file))
+file_lyrdb = os.path.join(path,gds_file+'.lyrdb')
 num_errors = layout_check(cell = top_cell, verbose=True, GUI=True, file_rdb=file_lyrdb)
 
-print('SiEPIC_EBeam_PDK: ebeam_adiabatic_te1550 - done')
+print('SiEPIC_EBeam_PDK: {} - done'.format(gds_file))
 
 # Print the result value to standard output
 print(f"::set-output name=result::{num_errors}")
